b4d8a91c65c2ce6819267e7d9ba27a5340679754
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-17 10:26:25 -0300

    operadores básicos

---
+++ JavaScript/aula001.js
@@ -0,0 +1,36 @@
+let num = 10
+
+console.log(num)
+
+num = 20
+
+console.log(num)
+
+num += 10
+
+console.log(num)
+
+num -= 10
+
+console.log(num)
+
+num *= 10
+
+console.log(num)
+
+num /= 10
+
+console.log(num)
+
+const combValue = 5.79
+const mediaKm = 12
+const distancia = 1580
+
+const consumido = distancia / mediaKm
+let custoTotal = consumido * combValue
+
+
+//let medCombustivel = mediaValueKm / combValue
+//let custoTotal = medCombustivel * distancia
+
+console.log(custoTotal.toFixed(2))
